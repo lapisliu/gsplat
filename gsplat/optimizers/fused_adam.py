b3530fe6816770b378e85f1f7d0ca682560b1cd4
lapisliu <lxteng2016@126.com>
2024-12-31 15:54:28 -0500

    chore: logging

--- gsplat/optimizers/fused_adam.py
+++ gsplat/optimizers/fused_adam.py
@@ -24,8 +24,8 @@ class FusedAdamMultiTensor(torch.optim.Optimizer):
         group_idx = 0
         tot_num_elems = 0
         step = 0
+        param_to_group = [0]  # param_list[i] belongs to group j if param_to_group[j] <= i < param_to_group[j+1]
 
-        print(f"param_groups len:", len(self.param_groups))
         for group in self.param_groups:
             lr = group['lr']
             beta_1, beta_2 = group['beta_1'], group['beta_2']
@@ -38,7 +38,6 @@ class FusedAdamMultiTensor(torch.optim.Optimizer):
             eps_list.append(epsilon)
             weight_decay_list.append(weight_decay)
 
-            print(f"group len: {len(group['params'])}")
             for p in group['params']:
                 if p.grad is None:
                     continue
@@ -55,35 +54,31 @@ class FusedAdamMultiTensor(torch.optim.Optimizer):
                 state['step'] += 1
 
                 param_list.append(p.data.contiguous())
-                print(f"p.data.shape: {p.data.shape}")
                 print(f"param_list len: {len(param_list)}")
                 grad_list.append(p.grad.contiguous())
-                print(f"p.grad.shape: {p.grad.shape}")
                 print(f"grad_list len: {len(grad_list)}")
                 exp_avg_list.append(exp_avg.data.contiguous())
-                print(f"exp_avg.shape: {exp_avg.shape}")
                 print(f"exp_avg_list len: {len(exp_avg_list)}")
                 exp_avg_sq_list.append(exp_avg_sq.data.contiguous())
-                print(f"exp_avg_sq.shape: {exp_avg_sq.shape}")
                 print(f"exp_avg_sq_list len: {len(exp_avg_sq_list)}")
                 step = state['step']
 
                 tot_num_elems += p.numel()
                 tensor_to_group.append(group_idx)
 
+            param_to_group.append(len(param_list))
+
             group_idx += 1
 
         if hasattr(self, 'verbose') and self.verbose:
             print(f"Launching fused kernel with {tot_num_elems} elements and {len(param_list)} parameters.")
 
-        # debug lr
-        print(f"lr: {lr_list}")
-        print(f"beta_1: {beta_1_list}")
-        print(f"beta_2: {beta_2_list}")
-        print(f"eps: {eps_list}")
-        print(f"weight_decay: {weight_decay_list}")
         print(f"tot_num_elems: {tot_num_elems}")
-        print(f"tensor_to_group: {tensor_to_group}")
+        print(f"param_to_group: {param_to_group}")
+        print(f"param_list len: {len(param_list)}")
+        print(f"grad_list len: {len(grad_list)}")
+        print(f"exp_avg_list len: {len(exp_avg_list)}")
+        print(f"exp_avg_sq_list len: {len(exp_avg_sq_list)}")
         print(f"step: {step}")
         fuse_adam_step_multi_tensor(
             [param_list, grad_list, exp_avg_list, exp_avg_sq_list], step,
